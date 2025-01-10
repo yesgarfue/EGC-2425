d5a2f252cbecdb3310282eb2fc236491033a0068
Yesica Garate <yesgarfue@alum.us.es>
2025-01-10 16:54:49 +0100

    fix(init-bug): Solucionado el error de desplegue debido a comentar la funcion create_app  - Solucionado el error presente en la rama feature/init-bug

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,6 @@ load_dotenv()
 db = SQLAlchemy()
 migrate = Migrate()
 
-'''
 def create_app(config_name='development'):
     app = Flask(__name__)
 
@@ -65,6 +64,5 @@ def create_app(config_name='development'):
         }
 
     return app
-'''
 
 app = create_app()
